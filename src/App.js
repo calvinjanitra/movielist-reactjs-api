229959b8f38eaffe275477aec9f743a7d0b3482f
Calvin Janitra <calvinjanitra9@gmail.com>
2023-02-11 23:27:37 +0700

    add file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { getMovieList, searchMovie } from './api'
 import NavigationBar from './components/navbar'
-import Intro from './components/intro'
 import { useEffect, useState } from 'react'
 
 const App = () => {
@@ -18,8 +17,8 @@ const App = () => {
         return nowMovies.map((movie, i) => {
             return (
                 <div className='Movie-wrapper' key={i}>
+                    <img className='Movie-image' alt='' src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
                     <div className='Movie-title'>{movie.title}</div>
-                    <img className='Movie-image' src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
                     <div className='Movie-date'>{movie.release_date}</div>
                     <div className='Movie-rate'>{movie.vote_average}</div>
                 </div>
@@ -27,13 +26,21 @@ const App = () => {
         })
     }
 
-    console.log({ nowMovies: nowMovies })
+    // console.log({ nowMovies: nowMovies })
+    const search = async (q) => {
+        const query = await searchMovie(q)
+        // setNowMovies(query.result)
+        console.log({ query: query })
+    }
+
     return (
         <div className='App'>
             <NavigationBar />
 
             <div className='App-header'>
-                <Intro />
+                <div className='intro text-white '>
+                    <input placeholder='Search Movies...' className='Movie-search' onChange={({ target }) => search(target.value)} />
+                </div>
                 <div className='Movie-container'>
                     <NowMoviesList />
                 </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,19 +1,19 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav } from 'react-bootstrap'
 
 const NavigationBar = () => {
     return (
         <div>
-            <Navbar bg="danger">
+            <Navbar bg='danger'>
                 <Container>
-                    <Navbar.Brand className="fw-bold">MOVIES LIST</Navbar.Brand>
+                    <Navbar.Brand className='fw-bold fs-3 '>MOVIES LIST</Navbar.Brand>
                     <Nav>
-                        <Nav.Link className="fw-bold">TV Series</Nav.Link>
-                        <Nav.Link className="fw-bold">Genres</Nav.Link>
+                        <Nav.Link className='fw-bold fs-4'>TV Series</Nav.Link>
+                        <Nav.Link className='fw-bold fs-4'>Genres</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
         </div>
-    );
-};
+    )
+}
 
-export default NavigationBar;
+export default NavigationBar
