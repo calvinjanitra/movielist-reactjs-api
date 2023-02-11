4b1e7521d8b9a0ef70fa8a75e31910f30ac89a24
Calvin Janitra <calvinjanitra9@gmail.com>
2023-02-11 16:12:50 +0700

    Fix API

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import "./App.css";
-import "./style/landing.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { getMovieList, searchMovie } from "./api";
-import NavigationBar from "./components/navbar";
-import Intro from "./components/intro";
-import { useEffect } from "react";
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { getMovieList, searchMovie } from './api'
+import NavigationBar from './components/navbar'
+import Intro from './components/intro'
+import { useEffect, useState } from 'react'
 
-function App() {
+const App = () => {
+    const [nowMovies, setNowMovies] = useState([])
+
     useEffect(() => {
-        getMovieList();
-    }, []);
+        getMovieList().then((result) => {
+            setNowMovies(result)
+        })
+    }, [])
 
+    const NowMoviesList = () => {
+        return nowMovies.map((movie, i) => {
+            return (
+                <div className='Movie-wrapper' key={i}>
+                    <div className='Movie-title'>{movie.title}</div>
+                    <img className='Movie-image' src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
+                    <div className='Movie-date'>{movie.release_date}</div>
+                    <div className='Movie-rate'>{movie.vote_average}</div>
+                </div>
+            )
+        })
+    }
+
+    console.log({ nowMovies: nowMovies })
     return (
-        <div className="App">
+        <div className='App'>
             <NavigationBar />
 
-            <div className="App-header">
+            <div className='App-header'>
                 <Intro />
-
-                <div className="Movie-container">
-                    <div className="Movie-wrapper">
-                        <div className="Movie-title">MOVIE TITLE</div>
-                        <img className="Movie-image"></img>
-                        <div className="Movie-date">11-2-2023</div>
-                        <div className="Movie-rate">8.9</div>
-                    </div>
+                <div className='Movie-container'>
+                    <NowMoviesList />
                 </div>
             </div>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
--- src/components/intro.js
+++ src/components/intro.js
@@ -1,14 +1,13 @@
-import { Col, Container, Row, Form } from "react-bootstrap";
-
+import '../App.css'
 const Intro = () => {
     const search = (q) => {
-        console.log({ q });
-    };
+        console.log({ q })
+    }
     return (
-        <div className="intro text-white ">
-            <input placeholder="Search Movies..." className="Movie-search" onChange={({ target }) => search(target.value)} />
+        <div className='intro text-white '>
+            <input placeholder='Search Movies...' className='Movie-search' onChange={({ target }) => search(target.value)} />
         </div>
-    );
-};
+    )
+}
 
-export default Intro;
+export default Intro
